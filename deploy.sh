032de96ee9355576b690392c8a518bb32029ea0a
Bracken Dawson <abdawson@gmail.com>
2019-09-13 20:31:53 +0100

    Wrong P

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,12 @@ echo -e $DEPLOY_KEY | base64 -d > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 set -x
 
-scp -p 6022 smib root@space.somakeit.org.uk:/usr/local/bin
+scp -P 6022 smib root@space.somakeit.org.uk:/usr/local/bin
 cp init-script slacker-smib
+set +x
 sed -i "s/<TOKEN>/$SLACK_TOKEN/" slacker-smib
-scp -p 6022 slacker-smib root@space.somakeit.org.uk:/etc/init.d
+set -x
+scp -P 6022 slacker-smib root@space.somakeit.org.uk:/etc/init.d
 ssh -p 6022 root@space.somakeit.org.uk service slacker-smib stop
 sleep 5
 ssh -p 6022 root@space.somakeit.org.uk service slacker-smib start
