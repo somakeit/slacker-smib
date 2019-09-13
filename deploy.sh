94a38711a76c01c4d8e06d56096f20e1522d254d
Bracken Dawson <abdawson@gmail.com>
2019-09-13 20:19:59 +0100

    Try a different place for the ssk key
    I love getting travis working, so many commits!

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,14 @@
 
 set -e
 set +x
-echo -e $DEPLOY_KEY | base64 -d > ~/.ssh/id_deploy
-chmod 600 ~/.ssh/id_deploy
+echo -e $DEPLOY_KEY | base64 -d > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
 set -x
 
-scp -p 6022 smib root@space.somakeit.org.uk:/usr/local/bin/smib
-sed -i "s/<TOKEN>/$SLACK_TOKEN/" init-script
-scp -p 6022 smib root@space.somakeit.org.uk:/etc/init.d/slacker-smib
+scp -p 6022 smib root@space.somakeit.org.uk:/usr/local/bin
+cp init-script slacker-smib
+sed -i "s/<TOKEN>/$SLACK_TOKEN/" slacker-smib
+scp -p 6022 slacker-smib root@space.somakeit.org.uk:/etc/init.d
 ssh -p 6022 root@space.somakeit.org.uk service slacker-smib stop
 sleep 5
 ssh -p 6022 root@space.somakeit.org.uk service slacker-smib start
