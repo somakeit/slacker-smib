559e8e7ab5c9ed1418985e9d9b272e025b4465c0
Bracken Dawson <abdawson@gmail.com>
2019-09-13 20:38:38 +0100

    Allot ssh to actually connect

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,12 @@ echo -e $DEPLOY_KEY | base64 -d > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 set -x
 
-scp -P 6022 smib root@space.somakeit.org.uk:/usr/local/bin
+scp -o StrictHostKeyChecking=no -P 6022 smib root@space.somakeit.org.uk:/usr/local/bin
 cp init-script slacker-smib
 set +x
 sed -i "s/<TOKEN>/$SLACK_TOKEN/" slacker-smib
 set -x
-scp -P 6022 slacker-smib root@space.somakeit.org.uk:/etc/init.d
-ssh -p 6022 root@space.somakeit.org.uk service slacker-smib stop
+scp -o StrictHostKeyChecking=no -P 6022 slacker-smib root@space.somakeit.org.uk:/etc/init.d
+ssh -o StrictHostKeyChecking=no -p 6022 root@space.somakeit.org.uk service slacker-smib stop
 sleep 5
-ssh -p 6022 root@space.somakeit.org.uk service slacker-smib start
+ssh -o StrictHostKeyChecking=no -p 6022 root@space.somakeit.org.uk service slacker-smib start
