8eba73a3e7c4f82eb9df1dca3b440bf02a14a223
Bracken Dawson <abdawson@gmail.com>
2019-09-15 17:10:05 +0100

    Was getting message events with zero length text.

--- internal/smib/smib.go
+++ internal/smib/smib.go
@@ -46,62 +46,65 @@ func (s *SMIB) ListenAndRobot() error {
 }
 
 func (s *SMIB) handleMessage(message *slack.MessageEvent) error {
-	if message.Text[0] == '?' {
-		s.slack.SendMessage(s.slack.NewTypingMessage(message.Channel))
+	if len(message.Text) < 1 || message.Text[0] != '?' {
+		return nil
+	}
 
-		user, err := s.slack.GetUserInfo(message.User)
+	s.slack.SendMessage(s.slack.NewTypingMessage(message.Channel))
+
+	user, err := s.slack.GetUserInfo(message.User)
+	if err != nil {
+		return fmt.Errorf("failed to get user info: %s", err)
+	}
+
+	parts := strings.SplitN(message.Text, " ", 2)
+	cmd := strings.TrimPrefix(parts[0], "?")
+	args := ""
+	if len(parts) > 1 {
+		args = parts[1]
+	}
+
+	channel, err := s.slack.GetChannelInfo(message.Channel)
+	if err != nil {
+		// TODO handle DMs
+		return fmt.Errorf("failed to get channel info: %s", err)
+	}
+
+	output, err := s.cmd.Run(
+		cmd,
+		user.Name,
+		channel.Name,
+		args,
+	)
+	switch err := err.(type) {
+	case nil:
+		break
+	case command.NotFoundError:
+		s.slack.SendMessage(s.slack.NewOutgoingMessage(
+			fmt.Sprintf("Sorry %s, I don't have a %s command.", user.Name, cmd),
+			message.Channel,
+		))
+		return nil
+	case command.NotUniqueError:
+		s.slack.SendMessage(s.slack.NewOutgoingMessage(
+			fmt.Sprintf("Sorry %s, that wasn't unique, try one of: %s", user.Name, err.Commands()),
+			message.Channel,
+		))
+		return nil
+	default:
+		return err
+	}
+
+	reader := bufio.NewReader(output)
+	for {
+		out, err := reader.ReadString('\n')
+		if len(out) > 0 {
+			s.slack.SendMessage(s.slack.NewOutgoingMessage(out, message.Channel))
+		}
 		if err != nil {
-			return fmt.Errorf("failed to get user info: %s", err)
-		}
-
-		parts := strings.SplitN(message.Text, " ", 2)
-		cmd := strings.TrimPrefix(parts[0], "?")
-		args := ""
-		if len(parts) > 1 {
-			args = parts[1]
-		}
-
-		channel, err := s.slack.GetChannelInfo(message.Channel)
-		if err != nil {
-			// TODO handle DMs
-			return fmt.Errorf("failed to get channel info: %s", err)
-		}
-
-		output, err := s.cmd.Run(
-			cmd,
-			user.Name,
-			channel.Name,
-			args,
-		)
-		switch err := err.(type) {
-		case nil:
 			break
-		case command.NotFoundError:
-			s.slack.SendMessage(s.slack.NewOutgoingMessage(
-				fmt.Sprintf("Sorry %s, I don't have a %s command.", user.Name, cmd),
-				message.Channel,
-			))
-			return nil
-		case command.NotUniqueError:
-			s.slack.SendMessage(s.slack.NewOutgoingMessage(
-				fmt.Sprintf("Sorry %s, that wasn't unique, try one of: %s", user.Name, err.Commands()),
-				message.Channel,
-			))
-			return nil
-		default:
-			return err
-		}
-
-		reader := bufio.NewReader(output)
-		for {
-			out, err := reader.ReadString('\n')
-			if len(out) > 0 {
-				s.slack.SendMessage(s.slack.NewOutgoingMessage(out, message.Channel))
-			}
-			if err != nil {
-				break
-			}
 		}
 	}
+
 	return nil
 }
